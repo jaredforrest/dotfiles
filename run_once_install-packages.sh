777b48f9c9c82516d18f6b16bb6e5f2c4b9af623
Jared Forrest <jared_forrest@mailbox.org>
2021-01-28 16:04:09 +0000

    add systemd

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac
+
+systemctl --user daemon-reload
 systemctl enable --user --now dunst.service notify-time.timer offline-imap.timer sxhkd.service vdirsyncer.timer
