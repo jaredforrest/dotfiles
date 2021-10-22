66e738cb948623d37967b527dffc063cd5e7994b
jaredforrest <jared@jaredforrest.xyz>
2021-10-22 02:42:24 +0100

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac
 
+gidtown dwm-jared
+gidtown dmenu-jared
+gidtown st-jared
+gidtown tabbed-jared
+gidtown hnotify-utils
 systemctl --user daemon-reload
 systemctl enable --user --now dunst.service notify-time.timer offlineimap-oneshot.timer sxhkd.service vdirsyncer.timer
